a847f92065c52b010ad79bc01a24cf2cc6b59aa7
Enzo Marques 12TA <enzoflashiron@gmail.com>
2025-04-27 22:22:24 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,39 @@
+/* function mensagem(){
+    alert("Seja bem-vindo(a)!")
+    console.log("Seja bem-vindo(a)!")
+    console.log("Você é muito legal!")
+}
+
+mensagem()
+
+function somar(numero1, numero2){
+    console.log(numero1 + numero2)
+}
+
+let numero1 = 40
+let numero2 = 23
+
+somar(numero1, numero2)
+somar(30, 12)
+somar(19, 289)  
+
+*/  
+
+function raizQuadrada(numero){
+    console.log(Math.sqrt(numero))
+}
+
+function potenciacao(valor1, valor2){
+    console.log(Math.pow(valor1, valor2))
+}
+
+function multiplicacao(valor1, valor2){
+    console.log(valor1 * valor2)
+}
+
+let numero1 = Number(prompt("Digite o número 1:"))
+let numero2 = Number(prompt("Digite o número 2:"))
+
+raizQuadrada(numero1)
+potenciacao(numero1, numero2)
+multiplicacao(numero2, numero1)
