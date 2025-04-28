d01b1dccbc99b470713edecbfd494e08ff8d6ef0
Enzo Marques 12TA <enzoflashiron@gmail.com>
2025-04-27 22:42:01 -0300

    Segundo commit do JS

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-/* function mensagem(){
+/* 
+  ##  Exemplo 1:
+
+function mensagem(){
     alert("Seja bem-vindo(a)!")
     console.log("Seja bem-vindo(a)!")
     console.log("Você é muito legal!")
 }
 
 mensagem()
+*/ 
+
+/*
+## Exemplo 2:
 
 function somar(numero1, numero2){
     console.log(numero1 + numero2)
@@ -16,8 +23,10 @@ let numero2 = 23
 somar(numero1, numero2)
 somar(30, 12)
 somar(19, 289)  
+*/
 
-*/  
+/*
+ ## Exemplo 3:
 
 function raizQuadrada(numero){
     console.log(Math.sqrt(numero))
@@ -37,3 +46,35 @@ let numero2 = Number(prompt("Digite o número 2:"))
 raizQuadrada(numero1)
 potenciacao(numero1, numero2)
 multiplicacao(numero2, numero1)
+
+/*
+## Exemplo 4:
+
+
+function raizQuadrada(numero){
+    return Math.sqrt(numero)
+}
+
+function potenciacao(valor1, valor2){
+    return Math.pow(valor1, valor2)
+}
+
+function multiplicacao(valor1, valor2){
+    return valor1 * valor2
+}
+
+let numero1 = Number(prompt("Digite o número 1:"))
+let numero2 = Number(prompt("Digite o número 2:"))
+
+let raiz1 = raizQuadrada(numero1)
+let raiz2 = raizQuadrada(numero2)
+
+console.log(raiz1, raiz2)
+*/
+
+/* ## Exemplo 5:
+*/
+
+function boasVindas(){
+    console.log("Seja bem vindo(a)!")
+}
